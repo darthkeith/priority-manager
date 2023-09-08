a3ad96f1684b4ccac2f6ab914468dd402c389363
Keith Kaplan <keithkaplan0@gmail.com>
2023-09-08 02:06:48 -0700

    feat!: Replace leftist heap with pairing heap

--- heap.py
+++ heap.py
@@ -2,10 +2,10 @@
 
 Functions
 ---------
-init(is_higher: CompareStr, preorder: MaybeStrIter = None)
+init(is_higher: CompareStr, preorder: Iterator[str] = iter(['']))
     Initialize the heap.
 to_preorder() -> Iterator[str]
-    Generate the pre-order sequence of strings in the heap.
+    Return the pre-order sequence of strings in the heap.
 insert(key: str)
     Insert key into heap.
 delete(idx: int) -> str
@@ -18,20 +18,19 @@ is_empty() -> bool
     Check if heap is empty.
 is_valid_idx(idx: int) -> bool
     Check if number is a valid pre-order index.
-display() -> List[str]
-    Return list of rows of the visual display of the heap.
+display() -> Iterator[str]
+    Return the strings used to visually display the heap.
 
 Notes
 -----
-Implements a height-biased leftist heap.
+Implements a pairing heap as a child-sibling binary tree.
 """
 
-from math import log10
-from typing import Callable, Optional, Iterator, List
+from typing import Callable, Optional, Iterator
 
 # Type Aliases
 CompareStr =  Callable[[str, str], bool]
-MaybeIterStr =  Optional[Iterator[str]]
+MaybeNode = Optional['_Node']
 
 # Global Variables
 _is_higher: CompareStr = None
@@ -39,52 +38,43 @@ _root: '_Node' = None
 
 
 class _Node:
-    """Node in a height-biased leftist heap.
+    """Node in a pairing heap.
     
     Attributes
     ----------
     key : str
-        Key used for comparison.
-    left : _Node
-        Left child.
-    right : _Node
-        Right child.
-    rank : int
-        Minimum distance to null descendant.
+        Key used for comparison, assumed to be non-empty.
+    child : MaybeNode
+        First child.
+    sibling : MaybeNode
+        Next sibling.
     size : int
-        Number of nodes in subtree.
+        Number of nodes in child-sibling subtree.
     
     Notes
     -----
-    After construction, the right child of a node has a rank no bigger
-    than that of its left child (a null node has a rank of zero).
     Null nodes are represented by a value of `None`.
     """
 
-    def __init__(self, key: str, left: '_Node', right: '_Node') -> '_Node':
-        # Construct node with given key and children.
-        if right and left:
-            if left.rank < right.rank:
-                left, right = right, left
-            rank = 1 + right.rank
-            size = 1 + left.size + right.size 
-        else:
-            if right:
-                left, right = right, left
-            rank = 1
-            size = 1 + left.size if left else 1
+    def __init__(self,
+                 key: str,
+                 child: MaybeNode = None,
+                 sibling: MaybeNode = None):
+        # Construct node with given key, first child, and next sibling.
         self.key = key
-        self.left = left
-        self.right = right
-        self.rank : int = rank
-        self.size : int = size
+        self.child = child
+        self.sibling = sibling
+        self.size = 1
+        self.size += child.size if child else 0
+        self.size += sibling.size if sibling else 0
 
-    def copy(self, left: '_Node', right: '_Node') -> '_Node':
-        # Construct new node with same key and given children.
-        return _Node(self.key, left, right)
+    def with_sibling(self, sibling: MaybeNode) -> '_Node':
+        # Return a copy of the node with the given next sibling.
+        return _Node(self.key, self.child, sibling)
 
 
-def init(is_higher: CompareStr, preorder: MaybeIterStr = None):
+def init(is_higher: CompareStr,
+         preorder: Iterator[str] = iter([''])):
     """Initialize the heap.
 
     Must be called before the other functions in the module are used.
@@ -92,98 +82,111 @@ def init(is_higher: CompareStr, preorder: MaybeIterStr = None):
     Parameters
     ----------
     is_higher : CompareStr
-        Callback function used to order strings that returns `True` when the
-        first string has a higher priority than the second.
-    preorder : MaybeIterStr, default=None
+        Callback function used to order strings indicating whether the first
+        string has a higher priority than the second.
+    preorder : MaybeIterStr, default=iter([''])
         A pre-order sequence of strings, where an empty string represents
-        a null node (the default is None, which indicates an empty heap).
+        a null node.
     """
     global _is_higher
     global _root
     _is_higher = is_higher
-    def make_heap() -> Optional[_Node]:
+    def build_heap() -> MaybeNode:
         key = next(preorder)
         if key == '':
             return None
-        left = make_heap()
-        right = make_heap()
-        return _Node(key, left, right)
-    _root = make_heap() if preorder else None
+        child = build_heap()
+        sibling = build_heap()
+        return _Node(key, child, sibling)
+    _root = build_heap()
 
 
 def to_preorder() -> Iterator[str]:
-    """Return iterator of pre-order sequence of strings in the heap.
+    """Return the pre-order sequence of strings in the heap.
 
-    Returns
-    -------
-    Iterator[str]
-        The pre-order sequence of strings in the heap, where each null
-        node is represented by an empty string (if a node has an empty string
-        as its key, a single space character is used instead).
+    Each null node is represented by an empty string.
     """
-    def key_gen(node: Optional[_Node]) -> Iterator[str]:
+    def preorder(node: MaybeNode) -> Iterator[str]:
         if node == None:
             yield ''
             return
-        yield ' ' if node.key == '' else node.key
-        for k in key_gen(node.left):
+        yield node.key
+        for k in preorder(node.child):
             yield k
-        for k in key_gen(node.right):
+        for k in preorder(node.sibling):
             yield k
-    return key_gen(_root)
+    return preorder(_root)
+
+
+def _merge_pair(x: _Node, y: _Node) -> _Node:
+    # Merge two heaps and return the new root (discard sibling references).
+    parent, child = (y, x) if _is_higher(y.key, x.key) else (x, y)
+    new_child = child.with_sibling(parent.child)
+    return _Node(parent.key, new_child, None)
 
 
 def insert(key: str):
     """Insert key into heap."""
-    def do_insert(node: Optional[_Node]) -> _Node:
-        # Insert key into subtree and return new root.
-        if node == None:
-            return _Node(key, None, None)
-        if _is_higher(key, node.key):
-            return _Node(key, node, None)
-        right = do_insert(node.right)
-        return node.copy(node.left, right)
     global _root
-    _root = do_insert(_root)
+    if _root:
+        _root = _merge_pair(_root, _Node(key))
+    else:
+        _root = _Node(key)
+
+
+def _pair_siblings(node: MaybeNode) -> MaybeNode:
+    # Merge siblings pairwise.
+    if node == None or node.sibling == None:
+        return node
+    sibling = node.sibling.sibling
+    new_node =_merge_pair(node, node.sibling)
+    return new_node.with_sibling(_pair_siblings(sibling))
+
+
+def _merge_siblings(node: _Node) -> _Node:
+    # Recursively merge siblings pairwise into a single heap.
+    if node.sibling == None:
+        return node
+    return _merge_siblings(_pair_siblings(node))
+
+
+def _concat_siblings(left: MaybeNode, right: MaybeNode) -> MaybeNode:
+    # Concatenate two sibling lists.
+    if left == None:
+        return right
+    sibling = _concat_siblings(left.sibling, right)
+    return left.with_sibling(sibling)
 
 
 def delete(idx: int) -> str:
     """Delete node with given pre-order index and return its key."""
-    def merge(x: _Node, y: _Node) -> _Node:
-        # Merge two non-empty subtrees.
-        parent, child = (y, x) if _is_higher(y.key, x.key) else (x, y)
-        if parent.right == None:
-            return parent.copy(parent.left, child)
-        right = merge(parent.right, child)
-        return parent.copy(parent.left, right)
-    key = None
-    def do_delete(node: _Node, curr_idx: int) -> Optional[_Node]:
-        # Perform deletion from current node/index, return new root.
-        if curr_idx == idx:
+    global _root
+    if idx == 0:
+        key = _root.key
+        _root = _merge_siblings(_root.child) if _root.child else None
+        return key
+    key = ''
+    def do_delete(idx: int, node: _Node) -> MaybeNode:
+        # Delete node at pre-order index in subtree and return new root.
+        if idx == 0:
             nonlocal key
             key = node.key
-            if node.right == None:
-                return node.left
-            return merge(node.left, node.right)
-        left_idx = 1 + curr_idx
-        right_idx = left_idx + node.left.size
+            return _concat_siblings(node.child, node.sibling)
+        right_idx = 1 + (node.child.size if node.child else 0)
         if idx < right_idx:
-            left = do_delete(node.left, left_idx)
-            return node.copy(left, node.right)
-        right = do_delete(node.right, right_idx)
-        return node.copy(node.left, right)
-    global _root
-    _root = do_delete(_root, 0)
+            child = do_delete(idx - 1, node.child)
+            return _Node(node.key, child, node.sibling)
+        else:
+            sibling = do_delete(idx - right_idx, node.sibling)
+            return node.with_sibling(sibling)
+    _root = do_delete(idx, _root)
     return key
 
 
 def move(idx: int) -> str:
     """Delete then reinsert key of node with given pre-order index.
 
-    Returns
-    -------
-    key : str
-        Key of target node.
+    Return key of target node.
     """
     key = delete(idx)
     insert(key)
@@ -192,18 +195,16 @@ def move(idx: int) -> str:
 
 def rename(idx: int, name: str):
     """Rename item with given pre-order index."""
-    def do_rename(node: _Node, curr_idx: int):
-        # Perform rename from current node/index.
-        if curr_idx == idx: 
+    def do_rename(idx: int, node: _Node):
+        if idx == 0:
             node.key = name
             return
-        left_idx = 1 + curr_idx
-        right_idx = left_idx + node.left.size
+        right_idx = 1 + (node.child.size if node.child else 0)
         if idx < right_idx:
-            do_rename(node.left, left_idx)
+            do_rename(idx - 1, node.child)
         else:
-            do_rename(node.right, right_idx)
-    do_rename(_root, 0)
+            do_rename(idx - right_idx, node.sibling)
+    do_rename(idx, _root)
 
 
 def is_empty() -> bool:
@@ -218,49 +219,29 @@ def is_valid_idx(idx: int) -> bool:
     return idx >= 0 and idx < _root.size
 
 
-def display() -> List[str]:
-    """Return list of rows of the visual display of the heap.
-
-    Returns
-    -------
-    List[str]
-        List of rows of the text-based visual display of the heap.
-    """
+def display() -> Iterator[str]:
+    """Return the strings used to visually display the heap."""
     if is_empty():
-        return []
-    if _root.size == 1:
-        ndigits = 1
-    else:
-        ndigits = 1 + int(log10(_root.size - 1))
-    row_list = []
-    def build_rows(node: _Node, prefix: str, is_last_child: bool):
-        """Construct rows to append to `row_list`.
-
-        Parameters
-        ----------
-        node : _Node
-            Current node.
-        prefix : str
-            String used to draw the tree structure on the next row.
-        is_last_child : bool
-            True if node is a right child or an only child.
-        """
-        idx_str = "{:>{}}".format(len(row_list), ndigits)
-        tree_str = prefix
-        if is_last_child:
-            tree_str += '╚'
-            prefix += ' '
-        else:
-            tree_str += '╠'
-            prefix += '║'
-        is_leaf = node.left == None
-        tree_str += '═' if is_leaf else '╦'
-        row_list.append(idx_str + tree_str + node.key)
-        if is_leaf:
+        return iter([])
+    num_digits = len(str(_root.size - 1))
+    idx = 0
+    def do_display(node: MaybeNode, prefix: str) -> Iterator[str]:
+        if node == None:
             return
-        build_rows(node.left, prefix, node.right == None)
-        if node.right:
-            build_rows(node.right, prefix, True)
-    build_rows(_root, '', True)
-    return row_list
+        nonlocal idx
+        idx_str = f"{idx:>{num_digits}}"
+        idx += 1
+        c2 = '╦' if node.child else '═'
+        if node.sibling:
+            c1 = '╠'
+            c0 = '║'
+        else:
+            c1 = '╚'
+            c0 = ' '
+        yield idx_str + prefix + c1 + c2 + node.key
+        for row in do_display(node.child, prefix + c0):
+            yield row
+        for row in do_display(node.sibling, prefix):
+            yield row
+    return do_display(_root, '')
 
